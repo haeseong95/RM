62da5f51d5d69ad159d6f9254841a7c7f6cea44a
DESKTOP-D5SLV9K\xogns <heasoung9441@gmail.com>
2024-05-06 03:53:03 +0900

    리스트뷰 작동O

--- app/src/main/java/com/example/rm/CategoryInfo.java
+++ app/src/main/java/com/example/rm/CategoryInfo.java
@@ -2,11 +2,14 @@ package com.example.rm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,8 +26,8 @@ public class CategoryInfo extends AppCompatActivity {
     TextView categoryTitle, trashInfo;     // 쓰레기 종류
     ImageView btnBack;
     ListView listView;
-    ArrayList<TrashListData> arrayList = new ArrayList<>();
-    TrashAdapter trashAdapter = new TrashAdapter(CategoryInfo.this, arrayList); // 데이터-뷰 연결할 어댑터 생성
+    ArrayList<TrashListData> arrayList = new ArrayList<>();;     // TrashListData 클래스 사용
+    TrashAdapter trashAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,28 @@ public class CategoryInfo extends AppCompatActivity {
         trashInfo = (TextView)findViewById(R.id.trash_info);
         listView = (ListView)findViewById(R.id.category_listview);
 
+        categoryTitle.setText(getIntent().getStringExtra("category"));      // 쓰레기 종류의 이름을 상단 툴바에 표시
+
+
+
+        trashAdapter = new TrashAdapter(this, arrayList); // 어댑터 생성, arrayList는 비어있는 상태
+        setListView();        // listview 설정
+        clickListviewItem();    // listview 목록 클릭하면 상세 페이지로 이동
+
+
+        // 리스트뷰의 높이를 계산에서 layout 크기를 설정
+        int totalHeight = 0;
+        for (int i = 0; i < trashAdapter.getCount(); i++){
+            View listItem = trashAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (trashAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,69 +68,23 @@ public class CategoryInfo extends AppCompatActivity {
             }
         });
 
-        categoryTitle.setText(getIntent().getStringExtra("category"));      // 쓰레기 종류의 이름을 상단 툴바에 표시
-        setListView();        // listview 설정
-        clickListviewItem();    // listview 목록 클릭하면 상세 페이지로 이동
-
-
-        /*
-
-        // 정보 추가하기
-        arrayList.add("List를 <문자열>로 선언했으니 무조건 문자열만 넣기");
-        arrayList.add("잠온다");
-        trashAdapter.notifyDataSetChanged();    // add로 추가한 윗 데이터를 저장함
-
-
-        listView.setOnClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(CategoryInfo.this, TrashDetail.class);
-                startActivity(intent);
-            }
-        });
-
-
-         */
-
-         /*
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("").build();
-        final TrashRetroService retroService = retrofit.create(TrashRetroService.class);
-
-        retroService.getTrashName(new Callback<List<TrashListData>>() {
-            @Override
-            public void onResponse(Response<List<TrashListData>> response, Retrofit retrofit1) {
-
-                if(!response.isSuccessful()){
-                    return;
-                }
-
-                List<TrashListData> data = response.body();
-
-                ArrayAdapter adapter = new ArrayAdapter(CategoryInfo.this, data);
-                listView.setAdapter(adapter);
-
-
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        });
-         */
-
-
-
 
     }
 
     // 리스트 뷰 설정
     public void setListView() {
-        trashAdapter.add(R.drawable.ci_responsive_design, "쓰레기 이름1", "설명1");
-        trashAdapter.add(R.drawable.ci_responsive_design, "쓰레기 이름2", "설명2");
+
+        // arrayList에 항목 추가
+        arrayList.add(new TrashListData("쓰레기 이름1", "설명2", R.drawable.ci_responsive_design));
+        arrayList.add(new TrashListData("이름2", "설명2", R.drawable.cat_battery));
+        arrayList.add(new TrashListData("쓰레기 이름1", "설명2", R.drawable.ci_responsive_design));
+        arrayList.add(new TrashListData("이름2", "설명2", R.drawable.cat_battery));
+        arrayList.add(new TrashListData("쓰레기 이름1", "설명2", R.drawable.ci_responsive_design));
+        arrayList.add(new TrashListData("이름2", "설명2", R.drawable.cat_battery));
         listView.setAdapter(trashAdapter);      // 리스트 뷰에 어댑터 붙임
         listView.setClickable(true);        // 목록 클릭O
     }
+
 
     // listview 목록 클릭하면 쓰레기 상세 페이지로 데이터 전달
     public void clickListviewItem() {
@@ -120,6 +99,7 @@ public class CategoryInfo extends AppCompatActivity {
             }
         });
     }
+
 
 
 
--- app/src/main/java/com/example/rm/TrashAdapter.java
+++ app/src/main/java/com/example/rm/TrashAdapter.java
@@ -10,13 +10,13 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 // Listview의 어댑터, Listview와 데이터 항목(TrashListData 객체 목록)-뷰 중간다리 역할
+// 어댑터 : 인터페이스, 데이터 관리 + 뷰 형태로 출력 -> 가공할 data, 어떤 형태로 item 구성할지 layout -> data + layout 매핑
 public class TrashAdapter extends ArrayAdapter<TrashListData> {
-
-    ArrayList<TrashListData> arrayList = new ArrayList<>();
 
     public TrashAdapter(@NonNull Context context, ArrayList<TrashListData> arrayList) {     // 현재 컨텍스트(nullX), 뷰 만들 때 사용할 레이아웃 리소스 파일
         super(context, 0, arrayList);
@@ -24,9 +24,7 @@ public class TrashAdapter extends ArrayAdapter<TrashListData> {
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {  // position(listview 항목)에 위치한 데이터 -> 리스트 뷰의 아이템으로 만들어줌
-
-        TrashListData trashListData = getItem(position);        // positon에 데이터 추가
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {  // position(listview 항목 위치)에 위치한 데이터 -> 리스트 뷰의 아이템으로 만들어줌
 
         if (convertView == null){   // 화면에 없어진 view 재사용
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.category_listview, parent, false);
@@ -36,24 +34,13 @@ public class TrashAdapter extends ArrayAdapter<TrashListData> {
         TextView listInfo = convertView.findViewById(R.id.list_trashInfo);
         ImageView listImage = convertView.findViewById(R.id.list_trashImage);
 
+        TrashListData trashListData = getItem(position);        // positon에 데이터 추가해 해당 위치의 데이터 가져옴
         listName.setText(trashListData.getTrashName());     // 각 뷰에 대한 데이터 전달
         listInfo.setText(trashListData.getTrashInfo());
         listImage.setImageResource(trashListData.getTrashImage());
 
         return convertView;
     }
-
-    // 아이템에 데이터 추가
-    public void add(int image, String name, String info){
-        TrashListData trashListData = new TrashListData();
-
-        trashListData.setTrashImage(image);
-        trashListData.setTrashName(name);
-        trashListData.setTrashInfo(info);
-
-        arrayList.add(trashListData);
-    }
-
 }
 
 // 쓰레기 listview에서 보여지는 각 아이템 항목의 데이터 모델 정의함 (get 데이터 반환, set 데이터 설정)
@@ -61,6 +48,13 @@ class TrashListData {
     public int trashImage = 0;  // 쓰레기 이미지
     public String trashName = "";   // 쓰레기 이름
     public String trashInfo = "";   // 쓰레기 분리수거 방법
+
+    public TrashListData(String name, String info, int image){  // 생성자
+        this.trashName = name;
+        this.trashInfo = info;
+        this.trashImage = image;
+    }
+
 
     public int getTrashImage() {return trashImage;}
     public void setTrashImage(int trashImage) {this.trashImage = trashImage;}
