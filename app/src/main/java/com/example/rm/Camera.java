b4a2104bcac402c2e3eeb139382eb48275f3b50d
hanuelbawi <gaeulh011@gmail.com>
2024-05-08 15:33:48 +0900

    카메랑

--- app/src/main/java/com/example/rm/Camera.java
+++ app/src/main/java/com/example/rm/Camera.java
@@ -3,156 +3,78 @@ package com.example.rm;
 import static com.gun0912.tedpermission.normal.TedPermission.create;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
 
 import com.gun0912.tedpermission.PermissionListener;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 public class Camera extends AppCompatActivity {
-
-    private static final int REQUEST_IMAGE_CAPTURE;
-
-    static {
-        REQUEST_IMAGE_CAPTURE = 672;
-    }
-
+    private static final int REQUEST_IMAGE_CAPTURE = 672;
+    private static final int RESULT_CODE = 22;
     private String imageFilePath;
     private Uri photoUri;
-    private ActivityResultLauncher<Intent> startActivityResult;
 
+    Button btn_capture;
+    ImageView imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera);
-
-        PermissionListener permissionListener = new PermissionListener() {
-            @Override
-            public void onPermissionGranted() {
-                Toast.makeText(getApplicationContext(), "권한이 허용됨", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onPermissionDenied(List<String> deniedPermissions) {
-                Toast.makeText(getApplicationContext(), "권한이 거부됨", Toast.LENGTH_SHORT).show();
-            }
-        };
-
+        btn_capture = findViewById(R.id.capture_btn);
+        imageview = findViewById(R.id.result_image);
+        //권한 체크
         create()
                 .setPermissionListener(permissionListener)
                 .setRationaleMessage("카메라 권한이 필요합니다.")
                 .setDeniedMessage("거부하셨습니다.")
                 .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
                 .check();
-
-        // ActivityResultLauncher 초기화
-        startActivityResult = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> handleActivityResult(result));
-
-        findViewById(R.id.capture_btn).setOnClickListener(new View.OnClickListener() {
+        btn_capture.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // 카메라 앱 실행을 위한 Intent 생성
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    // 이미지 파일 생성
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (photoFile != null) {
-                        // FileProvider를 사용하여 이미지 파일의 URI 생성
-                        photoUri = FileProvider.getUriForFile(Camera.this, "com.example.com.provider", photoFile);
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-
-                        // 카메라 앱 실행
-                        startActivityResult.launch(intent);
-                    }
-                }
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, RESULT_CODE);
             }
         });
+
+
     }
 
-    // onActivityResult 처리 메서드
-    private void handleActivityResult(ActivityResult result) {
-        if (result.getResultCode() == Activity.RESULT_OK) {
-            Bitmap bitmap = BitmapFactory.decodeFile(imageFilePath);
-            ExifInterface exif = null;
-            try {
-                exif = new ExifInterface(imageFilePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            int exifOrientation;
-            int exifDegree;
-
-            if (exif != null) {
-                exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-                exifDegree = exifOrientationToDegress(exifOrientation);
-            } else {
-                exifDegree = 0;
-            }
-
-            ((ImageView) findViewById(R.id.result_image)).setImageBitmap(rotate(bitmap, exifDegree));
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == RESULT_CODE && resultCode == RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(photo);
         }
-    }
-
-    // 이미지 파일 생성 메서드
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "TEST_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDir
-        );
-        imageFilePath = image.getAbsolutePath();
-        return image;
-    }
-
-    // EXIF 정보를 사용하여 이미지 회전 각도 계산
-    private int exifOrientationToDegress(int exifOrientation) {
-        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
-            return 90;
-        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
-            return 180;
-        } else if ((exifOrientation == ExifInterface.ORIENTATION_ROTATE_270)) {
-            return 270;
+        else {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
         }
-        return 0;
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
-    // 이미지 회전 메서드
-    private Bitmap rotate(Bitmap bitmap, int exifDegree) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(exifDegree);
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-    }
+    PermissionListener permissionListener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            Toast.makeText(getApplicationContext(), "권한이 허용됨", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+            Toast.makeText(getApplicationContext(), "권한이 거부됨", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
 }
