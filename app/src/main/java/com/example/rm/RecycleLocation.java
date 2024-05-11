46d208701cd09faeae13defeefe8d75c27c29236
hanuelbawi <gaeulh011@gmail.com>
2024-05-11 19:29:51 +0900

    오류 해결

--- app/src/main/java/com/example/rm/RecycleLocation.java
+++ app/src/main/java/com/example/rm/RecycleLocation.java
@@ -1,6 +1,5 @@
 package com.example.rm;
 
-<<<<<<< HEAD
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -18,24 +17,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
-=======
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.util.Base64;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-import androidx.annotation.Nullable;
->>>>>>> 458f2aa9835baff6179ff2a02a16123d0b4e9c1b
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-<<<<<<< HEAD
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -81,28 +66,6 @@ public class RecycleLocation extends AppCompatActivity implements OnMapReadyCall
     private Location location;
 
     private View mLayout; // snackbar 사용하기 위함.
-=======
-import java.io.ByteArrayOutputStream;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
-public class RecycleLocation extends AppCompatActivity {
->>>>>>> 458f2aa9835baff6179ff2a02a16123d0b4e9c1b
-
-
-    // 테스트
-    ListView listView;
-    ArrayList<TrashMainListData> arrayList = new ArrayList<>();
-    TrashAdapter2 trashAdapter2;
-
-    // 이미지
-    private final int GALLERY = 1;
 
 
     @Override
@@ -110,15 +73,10 @@ public class RecycleLocation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
 
-<<<<<<< HEAD
         locationRequest = new LocationRequest()
                 .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
                 .setInterval(UPDATE_INTERVAL_MS)
                 .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
-=======
-        btnBack = (ImageView)findViewById(R.id.btn_back);
-        listView = findViewById(R.id.testlist);
->>>>>>> 458f2aa9835baff6179ff2a02a16123d0b4e9c1b
 
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
 
@@ -130,13 +88,8 @@ public class RecycleLocation extends AppCompatActivity {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        trashAdapter2 = new TrashAdapter2(this, arrayList);
-        test();
-        listView.setAdapter(trashAdapter2);
-
     }
 
-<<<<<<< HEAD
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
@@ -373,111 +326,6 @@ public class RecycleLocation extends AppCompatActivity {
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 15);
         mMap.moveCamera(cameraUpdate);
     }
-=======
-    // 이미지
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-    }
-
-    // 이미지 업로드
-    private void upLoadImage(Bitmap bitmap){
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-        String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
-        String imgname = String.valueOf(Calendar.getInstance().getTimeInMillis());
-
-    }
-
-
-    private void test(){
-        Call<List<RetroUser>> call = RetroClient.getRetroService().getImage();
-        call.enqueue(new Callback<List<RetroUser>>() {
-            @Override
-            public void onResponse(Call<List<RetroUser>> call, Response<List<RetroUser>> response) {
-                if (response.isSuccessful()) {
-
-                    int id = 0;
-                    String title, url = null;
-                    List<RetroUser> retroUser = response.body();
-
-                    for (int i=0; i<7; i++){
-                        RetroUser user = retroUser.get(i);
-
-                        title = user.getTitle();
-                        id = user.getId();
-                        url = user.getUrl();
-                        arrayList.add(new TrashMainListData(url, title));  // arrayList에 항목 추가
-                        Log.i("category 리스트뷰 전달 성공", "title : " + title + ", body : "+ url);
-                    }
-
-                    trashAdapter2.notifyDataSetChanged();    // listview 리스트의 크기+아이템 둘 다 변경될 때 사용 (=리스트 업데이트)
-                }
-                else {
-                    Log.e("category 리스트뷰", "리스트뷰 출력 실패");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<RetroUser>> call, Throwable t) {
-
-            }
-        });
-
-    }
-
-
-    /*
-    private void setText(){
-        Call<RetroApi> call = RetroClient.getRetroService().setCategoryMain();
-
-        call.enqueue(new Callback<RetroApi>() {
-            @Override
-            public void onResponse(Call<RetroApi> call, Response<RetroApi> response) {
-                ArrayList<RetroWriting> retroWritings = response.body().getRetroWriting();
-
-                String name, info, image;
-
-                if(response.isSuccessful()) {
-                    for (RetroWriting writing : retroWritings) {
-                        name = writing.getTitle();
-                        info = writing.getContent();
-                        image = writing.getImage();
-                        arrayList.add(new TrashMainListData(image, name, info));
-                        Log.i("지도, 리스트뷰 테스트", "image : " + image + ", name : " + name + ", info" + info);
-                    }
-
-                    trashAdapter2.notifyDataSetChanged();
-
-                    int totalHeight = 0;
-                    for (int i = 0; i < trashAdapter2.getCount(); i++){
-                        View listItem = trashAdapter2.getView(i, null, listView);
-                        listItem.measure(0, 0);
-                        totalHeight += listItem.getMeasuredHeight();
-                    }
-                    ViewGroup.LayoutParams params = listView.getLayoutParams();
-                    params.height = totalHeight + (listView.getDividerHeight() * (trashAdapter2.getCount() - 1));
-                    listView.setLayoutParams(params);
-                }else {
-                    Log.e("리스트뷰", "리스트뷰 출력 실패");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<RetroApi> call, Throwable t) {
-                Log.e("리스트뷰", "예외, 네트워크 오류 등", t);
-            }
-        });
-
-
-    }
-
-     */
-
-
-
->>>>>>> 458f2aa9835baff6179ff2a02a16123d0b4e9c1b
 }
 
 
