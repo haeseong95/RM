75bb5bbe269b18ed57503760069cf7280058afc0
haeseong95 <haeseonguo@gmail.com>
2024-05-17 11:58:37 +0900

    지도 수정(안됨)

--- app/src/main/java/com/example/rm/RecycleLocation.java
+++ app/src/main/java/com/example/rm/RecycleLocation.java
@@ -41,9 +41,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
 public class RecycleLocation extends AppCompatActivity implements OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
 
     private GoogleMap mMap;
@@ -53,25 +50,20 @@ public class RecycleLocation extends AppCompatActivity implements OnMapReadyCall
     private static final int GPS_ENABLE_REQUEST_CODE = 2001;
     private static final int UPDATE_INTERVAL_MS = 1000; // 1초
     private static final int FASTEST_UPDATE_INTERVAL_MS = 500; // 0.5초
-
-    // OnRequestPermissionsResultCallback에서 수신된 결과에서 ActivityCompat.OnRequestPermissionsResultCallback를 사용한 퍼미션 요청을 구별하기 위함
     private static final int PERMISSION_REQUEST_CODE = 100;
+
     boolean needRequest = false;
 
-    // 앱을 실행하기 위해 필요한 퍼미션 정의
-    String[] REQUIRED_PERMISSION = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+    String[] REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
 
     Location mCurrentLocation;
     LatLng currentPosition;
 
     private FusedLocationProviderClient mFusedLocationClient;
-    private LocationRequest locationRequest; // 주의
+    private LocationRequest locationRequest;
     private Location location;
 
     private View mLayout; // snackbar 사용하기 위함.
-
-    // private ApiService apiService; Class 선언 필요
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,96 +76,38 @@ public class RecycleLocation extends AppCompatActivity implements OnMapReadyCall
                 .setFastestInterval(FASTEST_UPDATE_INTERVAL_MS);
 
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
-
         builder.addLocationRequest(locationRequest);
-
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        // Retrofit 객체 초기화
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://your-api-url.com/") // 서버의 URL로 설정
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        // ApiService 인터페이스 구현체 생성
-        // apiService = retrofit.create(ApiService.class); // 맵 이용을 위한 API 서비스 설정 해야 함
-
-        // Map 초기화 코드...
-
+        mLayout = findViewById(R.layout.map); // Snackbar를 위한 메인 레이아웃 ID 할당
     }
-
-    /* private void getRecycleLocationsFromServer() {
-        Call<List<RecycleLocationModel>> call = apiService.getRecycleLocations();
-
-        call.enqueue(new Callback<List<RecycleLocationModel>>() {
-            @Override
-            public void onResponse(Call<List<RecycleLocationModel>> call, Response<List<RecycleLocationModel>> response) {
-                if (response.isSuccessful()) {
-                    List<RecycleLocationModel> recycleLocations = response.body();
-                    addRecycleLocationsToMap(recycleLocations);
-                } else {
-                    // 서버에서 오류 응답을 받은 경우 처리
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<RecycleLocationModel>> call, Throwable t) {
-                // 통신 실패 처리
-            }
-        });
-    }
-
-    private void addRecycleLocationsToMap(List<RecycleLocationModel> recycleLocations) {
-        for (RecycleLocationModel location : recycleLocations) {
-            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(latLng)
-                    .title(location.getName())
-                    .snippet(location.getDescription());
-            mMap.addMarker(markerOptions);
-        }
-    } */  // 서버 연결을 위한 코드
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-
         Log.d(TAG, "onMapReady: 들어옴 ");
-
         mMap = googleMap;
 
-        // 지도의 초기위치 이동
         setDefaultLocation();
-        // 주변의 분리수거 장소
-        // addRecycleLocations();
 
-        // 런타임 퍼미션 처리
-        // 1. 위치 퍼미션을 가지고 있는지 확인합니다.
         int hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
         int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
 
         if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
-            // 2. 이미 퍼미션을 가지고 있다면
-            startLocationUpdates(); // 3. 위치 업데이트 실행
+            startLocationUpdates();
         } else {
-            // 2. 퍼미션 요청을 허용한 적 없다면 퍼미션 요청하기
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSION[0])) {
-                // 요청 진행하기 전에 퍼미션이 왜필요한지 설명
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
                 Snackbar.make(mLayout, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-
-                        // 사용자에게 퍼미션 요청, 요청 결과는 onRequestPermisionResult에서 수신
-                        ActivityCompat.requestPermissions(RecycleLocation.this, REQUIRED_PERMISSION, PERMISSION_REQUEST_CODE);
+                        ActivityCompat.requestPermissions(RecycleLocation.this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
                     }
                 }).show();
             } else {
-                // 사용자가 퍼미션 거부를 한적이 없는 경우 퍼미션 요청을 바로 함.
-                // 요청 결과는 onRequestPermissionResult에서 수신된다.
-                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSION, PERMISSION_REQUEST_CODE);
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERMISSION_REQUEST_CODE);
             }
         }
 
@@ -186,67 +120,39 @@ public class RecycleLocation extends AppCompatActivity implements OnMapReadyCall
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            } else {
+                Toast.makeText(this, "권한 거부됨. 앱 기능이 제한됩니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     LocationCallback locationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(@NonNull LocationResult locationResult) {
             super.onLocationResult(locationResult);
 
             List<Location> locationList = locationResult.getLocations();
-
-            if (locationList.size() > 0) {
+            if (!locationList.isEmpty()) {
                 location = locationList.get(locationList.size() - 1);
-
                 currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
 
                 String markerTitle = getCurrentAddress(currentPosition);
-                String markerSnippet = "위도 :" + String.valueOf(location.getLatitude()) + "경도 :" +
-                        String.valueOf(location.getLongitude());
+                String markerSnippet = "위도:" + location.getLatitude() + ", 경도:" + location.getLongitude();
 
-                // 현재 위치에 마커 생성하고 이동
                 setCurrentLocation(location, markerTitle, markerSnippet);
-
                 mCurrentLocation = location;
             }
         }
     };
 
-
-    private String getCurrentAddress(LatLng currentPosition) {
-        // 지오코더 gps를 주소로 변환
-
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
-
-        List<Address> addresses;
-
-        try {
-            addresses = geocoder.getFromLocation(
-                    currentPosition.latitude,
-                    currentPosition.longitude,
-                    1
-            );
-        } catch (IOException ioException) {
-            // 네트워크 문제
-            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
-            return "지오코더 서비스 사용 불가";
-        } catch (IllegalArgumentException illegalArgumentException) {
-            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
-            return "잘못된 GPS 좌표";
-        }
-
-        if (addresses == null || addresses.size() == 0) {
-            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
-            return "주소 미발견";
-        } else {
-            Address address = addresses.get(0);
-            return address.getAddressLine(0).toString();
-        }
-    }
-
     private void setCurrentLocation(Location location, String markerTitle, String markerSnippet) {
-
-        if (currentMarker != null) {
-            currentMarker.remove();
-        }
+        if (currentMarker != null) currentMarker.remove();
 
         LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
 
@@ -262,88 +168,39 @@ public class RecycleLocation extends AppCompatActivity implements OnMapReadyCall
         mMap.moveCamera(cameraUpdate);
     }
 
-
     private void startLocationUpdates() {
         if (!checkLocationServicesStatus()) {
-            showDiologForLocationServiceSetting();
-
+            showDialogForLocationServiceSetting();
         } else {
-            int hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-            int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
-
-            if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED || hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
-
-                Log.d(TAG, "startLocationUpdates: 퍼미션 없음");
-                return;
-            }
-
-            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
             if (checkPermission()) {
-                mMap.setMyLocationEnabled(true);
-            }
-        }
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Log.d(TAG, "onStart: ");
-
-        if (checkPermission()) {
-            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
-
-            if (mMap != null) {
+                mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
                 mMap.setMyLocationEnabled(true);
             }
         }
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        if (mFusedLocationClient != null) {
-            mFusedLocationClient.removeLocationUpdates(locationCallback);
-        }
-    }
-
-
 
     private boolean checkPermission() {
-
         int hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
         int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
-
-        if (hasFineLocationPermission != PackageManager.PERMISSION_GRANTED || hasCoarseLocationPermission != PackageManager.PERMISSION_GRANTED) {
-
-            Log.d(TAG, "startLocationUpdates: 퍼미션 없음");
-            return false;
-
-        } else {
-            return true;
-        }
-
+        return hasFineLocationPermission == PackageManager.PERMISSION_GRANTED && hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED;
     }
 
-    private void showDiologForLocationServiceSetting() {
-
+    private void showDialogForLocationServiceSetting() {
         AlertDialog.Builder builder = new AlertDialog.Builder(RecycleLocation.this);
         builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다. 위치설정을 수정하시겠습니까?");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다. 위치 설정을 수정하시겠습니까?");
         builder.setCancelable(true);
         builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+            public void onClick(DialogInterface dialog, int id) {
                 Intent callGPSSettingIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
             }
         });
         builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.cancel();
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
             }
         });
         builder.create().show();
@@ -351,47 +208,64 @@ public class RecycleLocation extends AppCompatActivity implements OnMapReadyCall
 
     private boolean checkLocationServicesStatus() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
     private void setDefaultLocation() {
-
-        // 기본 위치
         LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
         String markerTitle = "위치 정보 가져올 수 없음";
         String markerSnippet = "위치 퍼미션과 GPS 활성 여부를 확인하세요";
 
-        if (currentMarker != null) {
-            currentMarker.remove();
-        }
+        if (currentMarker != null) currentMarker.remove();
 
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(DEFAULT_LOCATION);
         markerOptions.title(markerTitle);
         markerOptions.snippet(markerSnippet);
         markerOptions.draggable(true);
+
         currentMarker = mMap.addMarker(markerOptions);
 
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(DEFAULT_LOCATION, 15);
         mMap.moveCamera(cameraUpdate);
     }
 
-    /* private void addRecycleLocations() {
-        // 분리수거 장소들의 위치 정보를 리스트에 추가합니다. 여기서는 하드코딩된 예시를 사용하겠습니다.
-        List<LatLng> recycleLocations = new ArrayList<>();
-        recycleLocations.add(new LatLng(37.5665, 126.9780)); // 서울시청
-        recycleLocations.add(new LatLng(37.5664, 126.9779)); // 광화문
-        recycleLocations.add(new LatLng(37.5657, 126.9780)); // 덕수궁
-
-        // 각 분리수거 장소에 대해 마커를 지도에 추가합니다.
-        for (LatLng location : recycleLocations) {
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.position(location);
-            markerOptions.title("분리수거 장소");
-            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)); // 마커 아이콘을 녹색으로 설정합니다.
-            mMap.addMarker(markerOptions);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (checkPermission()) {
+            mFusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+            if (mMap != null) mMap.setMyLocationEnabled(true);
         }
-    } */ // 위치 마커 하드 코딩 (서버에서 API 연결 되는 코딩 필요)
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(locationCallback);
+        }
+    }
+
+    private String getCurrentAddress(LatLng currentPosition) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = geocoder.getFromLocation(currentPosition.latitude, currentPosition.longitude, 1);
+        } catch (IOException ioException) {
+            Toast.makeText(this, "지오코더 서비스 사용불가", Toast.LENGTH_LONG).show();
+            return "지오코더 서비스 사용 불가";
+        } catch (IllegalArgumentException illegalArgumentException) {
+            Toast.makeText(this, "잘못된 GPS 좌표", Toast.LENGTH_LONG).show();
+            return "잘못된 GPS 좌표";
+        }
+
+        if (addresses.isEmpty()) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_LONG).show();
+            return "주소 미발견";
+        } else {
+            Address address = addresses.get(0);
+            return address.getAddressLine(0);
+        }
+    }
 }
